0fd2ac55b36c23c5705a386da33fd8f88317281a
Daniel D Lundstrom <76541425+DDLund@users.noreply.github.com>
2022-09-16 12:46:37 -0700

    Update counterTest.m

--- counterTest.m
+++ counterTest.m
@@ -11,26 +11,28 @@ twoMoment = 0;
 kMoment = 0;
 Moment = 0;
 
+% runs test for the two counter and the k counter
+
 for i = 1:numTrials
     counter = dCounter(m);
-    tCounter = rTwoCounter(m,n,lambda,epsilon);   %comment out/ignore if k !=2
+    tCounter = rTwoCounter(m,n,lambda,epsilon);
     kCounter = rGeneralCounter(k,m,n,lambda,epsilon);
-    data = unidrnd(n,m,1);  % generate data
+    data = unidrnd(n,m,1);
     for j=1:length(data)
         counter = counter.input_data(data(j));
-        tCounter = tCounter.input_data(data(j));  %comment out/ignore if k !=2
+        tCounter = tCounter.input_data(data(j));
         kCounter = kCounter.input_data(data(j));
     end
-    Moment = counter.get_moment(k);
-    tMoment = tCounter.get_moment();              %comment out/ignore if k !=2
-    kMoment = kCounter.get_moment();
+    tMoment = counter.get_moment(2);
+    tMomentApprox = tCounter.get_moment();
+    kMoment = counter.get_moment(k);
+    kMomentApprox = kCounter.get_moment();
     
-    if abs(tMoment-Moment)<epsilon*Moment         %comment out/ignore if k !=2
+    if abs(tMomentApprox-tMoment)<epsilon*tMoment
         numSuccesses(1) = numSuccesses(1)+1;
     end
-    if abs(kMoment-Moment)<epsilon*Moment
+    
+    if abs(kMomentApprox-kMoment)<epsilon*kMoment
         numSuccesses(2) = numSuccesses(2)+1;
     end
 end
-
-
